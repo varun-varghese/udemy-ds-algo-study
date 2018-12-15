e3117e1e8acc609b0ed5aca3d1b31ea998684d5b
varun varghese <talktovarun@gmail.com>
2018-12-14 20:03:53 -0600

    int reverse

---
+++ src/main/java/dsalgo/learning/io/other/IntReverse.java
@@ -0,0 +1,40 @@
+package dsalgo.learning.io.other;
+
+import java.util.Stack;
+
+//import lombok.extern.slf4j.Slf4j;
+
+//@Slf4j
+public class IntReverse {
+	
+	public int reverse(int input) {
+		if (input == 0) {
+			return input;
+		} else {
+			Stack<Integer> intStack = new Stack<Integer>();
+			
+			int modInput = input < 0 ? -1 * input : input;
+			while (modInput > 0) {
+				int carry = modInput % 10;
+				intStack.push(carry);
+				modInput = modInput / 10;
+				//log.info("| modInput:{}, carry:{}", modInput, carry);
+			}
+			
+			//log.info("| intStack:{}", intStack);
+			
+			int result = 0;
+			int tenthsPow = 0;
+			while (!intStack.empty()) {
+				double zeroes = Math.pow(10, tenthsPow);
+				int topOfStack = intStack.pop();
+				result += (zeroes * topOfStack);
+				//log.info("| tenthsPow:{}, zeroes:{}, topOfStack:{}, result:{}", tenthsPow, zeroes, topOfStack, result);
+				tenthsPow++;
+			}
+			
+			return input < 0 ? (-1 * result) : result;
+		}
+	}
+
+}
---
+++ src/test/java/dsalgo/learning/io/other/IntReverseTest.java
@@ -0,0 +1,32 @@
+package dsalgo.learning.io.other;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntReverseTest {
+	
+	private IntReverse intReverse;
+
+	@Before
+	public void setUp() throws Exception {
+		intReverse = new IntReverse();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIntReverse() {
+		assertThat(intReverse.reverse(121), equalTo(121));
+		assertThat(intReverse.reverse(-121), equalTo(-121));
+		assertThat(intReverse.reverse(-321), equalTo(-123));
+		assertThat(intReverse.reverse(321), equalTo(123));
+		assertThat(intReverse.reverse(0), equalTo(0));
+	}
+
+}
